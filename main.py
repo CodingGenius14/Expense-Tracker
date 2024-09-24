3383b5da0dd18b8e7c719a465bb3ed60d8589e65
CodingGenius14 <voberai10@gmail.com>
2024-09-23 21:15:39 -0500

    expense tracker functionality finished. Need to work on saving data to a csv file and providing a summary of transactions

---
+++ main.py
@@ -0,0 +1,106 @@
+ACTION_PROMPT = "Enter (1) to add an expense | (2) to view expenses | (3) to delete an expense | (4) to reset your expenses | or (5) for a summary of your expenses: "
+DEL_CATEGORY_PROMPT = "Enter the Category you want to delete from: "
+
+
+def greeting():
+        print("""Welcome to Expense Tracker! Expense Tracker is designed to keep track of all your expenses.
+You can add expenses, delete expenses, view expenses, and clear all your expenses!\n""")
+
+
+expenses = {}
+
+
+def select_action():
+        action = input(ACTION_PROMPT)
+        while action not in ["1", "2", "3", "4"]:
+            print("Invalid input")
+            action = input(ACTION_PROMPT)
+        return action
+
+
+def adding_expense():
+        while True:
+            category = input("Enter the category of the expense (e.g, food, transportation, entertainment, E.T.C): ")
+
+            while True:
+                try: 
+                    value = float(input("Enter the expense value: "))
+                    break
+                except ValueError:
+                    print("Invalid input")
+
+
+
+            if category not in expenses:
+                expenses[category] = [value]
+            else:
+                expenses[category].append(value)
+        
+            choice = input("Enter (y) if you would like to add another expense or enter (n) to exit: ").lower()
+
+            if choice != "y":   
+                break
+            
+
+def show_expenses():
+    if not expenses:
+         print("Your expense tracker is empty.")
+         return
+
+    for category, entries in expenses.items():
+        print(f"Category: {category}")
+        for value in entries:
+            print(f" ${value}")
+
+
+def delete_expense():
+        show_expenses()
+        category = input(DEL_CATEGORY_PROMPT)
+
+        while category not in expenses:
+            print("Invalid input, category not found")
+            category = input(DEL_CATEGORY_PROMPT)
+
+        while True:
+            try:
+                value = float(input(f"Enter the value you want to delete from {category}: "))
+                if value not in expenses[category]:
+                     print(f"Invalid input, value not found in {category}")
+                else:
+                     break
+            except ValueError:
+                print("Invalid input, please enter a numeric value")
+            
+
+        print(f"Deleted {value} from {category}")
+        expenses[category].remove(value)
+        if not expenses[category]:
+            del expenses[category]
+            print(f"Deleted empty category: {category}")
+
+
+def reset_all_expenses():
+        print("All of your expenses have been cleared")
+        expenses.clear()
+
+
+def main(action):
+    if action == "1":
+        adding_expense()
+    elif action == "2":
+        show_expenses()
+    elif action == "3":
+        delete_expense()
+    elif action == "4":
+        reset_all_expenses()
+
+    
+greeting()
+
+while True:  
+    action = select_action() 
+    main(action)
+    program_control = input("Enter (y) to continue using the expense tracker or (e) to exit the program: ").lower()
+    if program_control == "e":
+            print("Thank you for using the expense tracker, have a great day!")
+            break
